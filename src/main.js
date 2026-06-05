a10ffb967f2a625b154cdcf2368637306fd1058d
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2026-06-05 08:55:44 +0200

    feat: lenin super cool feature

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import './style.css'
+import Lenis from 'lenis'
+
+// Initialize Lenis
+const lenis = new Lenis({
+    autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+    console.log(e);
+});
